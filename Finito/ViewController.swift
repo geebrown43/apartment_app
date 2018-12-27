08652d8773b5a6ed07daf3f813358e46024a5471
Gavin Brown <scareb15@gmail.com>
2018-12-27 13:12:30 -0700

    Initial Commit

---
+++ Finito/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Finito
+//
+//  Created by Gavin Brown on 12/27/18.
+//  Copyright © 2018 DevelopIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
